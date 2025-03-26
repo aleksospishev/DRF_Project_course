9bbf673d400c0af5fa21918fd463d0e1a13c7046
aleksospishev <aleksospishev@yandex.ru>
2025-03-26 19:35:22 +0300

    ci/cd

--- config/settings.py
+++ config/settings.py
@@ -71,7 +71,7 @@ DATABASES = {
         "USER": os.getenv("POSTGRES_USER"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
         "PORT": os.getenv("PORT"),
-        "HOST": os.getenv("POSTGRES_HOST"),
+        "HOST": os.getenv("POSTGRES_HOST", "localhost"),
     }
 }
 
